f8f534efec287b03d83cebf4857359b576672a10
mcpalez <palez.digital@gmail.com>
2023-06-16 13:47:25 +0200

    console error with createRoot fixed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import {createRoot} from 'react-dom/client';
+
 import App from './App';
 import '../node_modules/bootstrap/scss/bootstrap.scss';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
     <App />
 );
