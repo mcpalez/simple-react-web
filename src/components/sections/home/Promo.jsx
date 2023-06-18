1195eb38b47a65fd8f9d8a77639248df8655b12d
mcpalez <palez.digital@gmail.com>
2023-06-18 21:20:45 +0200

    customizing swiperjs, styling and temporarily content fill

--- src/components/sections/home/Promo.jsx
+++ src/components/sections/home/Promo.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from "swiper";
 import 'swiper/css';
 import "swiper/css/pagination";
+
+import Frame957 from '../../../assets/images/home/Frame957.jpeg';
 
 function Promo() {
   return (
@@ -14,7 +17,14 @@ function Promo() {
             <Row>
                 <Col md={12} className='promo-col__content'>
                     <Swiper
-                        slidesPerView={'auto'}
+                        breakpoints={{
+                            640: {
+                                slidesPerView: 1
+                            },
+                            1024: {
+                                slidesPerView: 2
+                            }
+                        }}
                         spaceBetween={30}
                         pagination={{
                         type: "progressbar",
@@ -24,20 +34,32 @@ function Promo() {
                         className="mySwiper"
                     >
                         <SwiperSlide>
-                            <h4>Intuitive interfaces and user-friendly experience</h4>
-                            <p>Website development from end-to-end refers to the process of building a website from the initial planning stages all the way through to launch and ongoing maintenance. <em className='underlined-text'>Go to UX/UI</em>.</p>
+                            <img src={Frame957} alt=''></img>
+                            <div className="slide-content">
+                                <h4>Intuitive interfaces and user-friendly experience</h4>
+                                <p className='p-default'>Website development from end-to-end refers to the process of building a website from the initial planning stages all the way through to launch and ongoing maintenance. <em className='underlined-text'>Go to UX/UI</em>.</p>
+                            </div>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <h4>Intuitive interfaces and user-friendly experience</h4>
-                            <p>Website development from end-to-end refers to the process of building a website from the initial planning stages all the way through to launch and ongoing maintenance. <em className='underlined-text'>Go to UX/UI</em>.</p>
+                            <img src={Frame957} alt=''></img>
+                            <div className="slide-content">
+                                <h4>Intuitive interfaces and user-friendly experience</h4>
+                                <p className='p-default'>Website development from end-to-end refers to the process of building a website from the initial planning stages all the way through to launch and ongoing maintenance. <em className='underlined-text'>Go to UX/UI</em>.</p>
+                            </div>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <h4>Intuitive interfaces and user-friendly experience</h4>
-                            <p>Website development from end-to-end refers to the process of building a website from the initial planning stages all the way through to launch and ongoing maintenance. <em className='underlined-text'>Go to UX/UI</em>.</p>
+                            <img src={Frame957} alt=''></img>
+                            <div className="slide-content">
+                                <h4>Intuitive interfaces and user-friendly experience</h4>
+                                <p className='p-default'>Website development from end-to-end refers to the process of building a website from the initial planning stages all the way through to launch and ongoing maintenance. <em className='underlined-text'>Go to UX/UI</em>.</p>
+                            </div>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <h4>Intuitive interfaces and user-friendly experience</h4>
-                            <p>Website development from end-to-end refers to the process of building a website from the initial planning stages all the way through to launch and ongoing maintenance. <em className='underlined-text'>Go to UX/UI</em>.</p>
+                            <img src={Frame957} alt=''></img>
+                            <div className="slide-content">
+                                <h4>Intuitive interfaces and user-friendly experience</h4>
+                                <p className='p-default'>Website development from end-to-end refers to the process of building a website from the initial planning stages all the way through to launch and ongoing maintenance. <em className='underlined-text'>Go to UX/UI</em>.</p>
+                            </div>
                         </SwiperSlide>
                     </Swiper>
                 </Col>
