5f6c099e2944881a0a1f81259dd1b0b16c5fab3e
mcpalez <palez.digital@gmail.com>
2023-06-20 23:17:35 +0200

    desktop dropdown menu link update

--- src/layout/NavigationBar.jsx
+++ src/layout/NavigationBar.jsx
@@ -39,15 +39,21 @@ function NavigationBar() {
 			<Container>
 				<nav className='navigation navbar--mini'>
 					<a href='/'><Logo/></a>
-					<div className="menu">
+					<div className='menu'>
 						<div className='list'>
-							<NavDropdown title="Services" id="basic-nav-dropdown">
+							<div className='list-dropdown--trigger'>Services</div>
+							<div className='list-dropdown' id='dropdown-list-menu'>
+								<Link className='dropdown-link' to='/services/development-services'>Development services</Link>
+								<Link className='dropdown-link' to='/services/ux-ui-services'>UX and UI services</Link>
+								<Link className='dropdown-link' to='/services'>See all services</Link>
+							</div>	
+							{/* <NavDropdown title="Services" id="basic-nav-dropdown">
 								<NavDropdown.Item>
 									<Link className='dropdown-link' to='/services/development-services'>Development services</Link>
 									<Link className='dropdown-link' to='/services/ux-ui-services'>UX and UI services</Link>
 									<Link className='dropdown-link' to='/services'>See all services</Link>
 								</NavDropdown.Item>
-							</NavDropdown>
+							</NavDropdown> */}
 							<Link to='/' className='nav-item'>About</Link>
 							<Link to='/' className='nav-item'>Work</Link>
 							<Link to='/' className='nav-item'>How to start?</Link>
@@ -78,24 +84,12 @@ function NavigationBar() {
 									<Link className='dropdown-link' to='/services'>See all services</Link>
 								</NavDropdown.Item>
 							</NavDropdown>
-							<li className="list-item">
-								<Link to='/' className='nav-item'>About</Link>
-							</li>
-							<li className="list-item">
-								<Link to='/' className='nav-item'>Work</Link>
-							</li>
-							<li className="list-item">
-								<Link to='/' className='nav-item'>How to start?</Link>
-							</li>
-							<li className="list-item">
-								<Link to='/' className='nav-item'>Resources</Link>
-							</li>
-							<li className="list-item">
-								<Link to='/' className='nav-item'>Contact</Link>
-							</li>
-							<li className="list-item">
-								<button className='button-primary-filled__navbar'>Start a project</button>
-							</li>
+							<li className="list-item"><Link to='/' className='nav-item'>About</Link></li>
+							<li className="list-item"><Link to='/' className='nav-item'>Work</Link></li>
+							<li className="list-item"><Link to='/' className='nav-item'>How to start?</Link></li>
+							<li className="list-item"><Link to='/' className='nav-item'>Resources</Link></li>
+							<li className="list-item"><Link to='/' className='nav-item'>Contact</Link></li>
+							<li className="list-item"><button className='button-primary-filled__navbar'>Start a project</button></li>
 						</ul>
 					</div>
 				</nav>
