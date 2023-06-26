83119b0f6f773e751f14ef606eb43131cdbb6ebb
mcpalez <palez.digital@gmail.com>
2023-06-26 23:29:50 +0200

    another section udpate on About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../components/pages/about/Hero';
+import Values from '../components/pages/about/Values';
 
 function About() {
   return (
     <main className='app-main about__page'>
         <Hero/>
+        <Values/>
     </main>
   )
 }
