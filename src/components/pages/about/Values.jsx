b8050bf53ae0cbd72d4bd8fbdf415cc3586a37db
mcpalez <palez.digital@gmail.com>
2023-07-02 23:53:23 +0200

    about values, section page

--- src/components/pages/about/Values.jsx
+++ src/components/pages/about/Values.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
 
 function Values() {
   return (
-    <div>Values</div>
+    <section className='values__about'>
+		<Container>
+			<Row>
+				<Col lg={12} className='values__col-introduction'>
+					<h3>Meet our core values</h3>
+					<p>We adore these ideas. These ideas guide our daily lives.</p>
+				</Col>
+			</Row>
+		</Container>
+    </section>
   )
 }
 
