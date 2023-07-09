eb9070cd8e713360ba9fccaa65502fbe7a8454f3
mcpalez <palez.digital@gmail.com>
2023-07-09 22:50:24 +0200

    responsive version of form is added, quick valid by css, text col to hero version on projectstart page

--- src/components/forms/ProjectForm.jsx
+++ src/components/forms/ProjectForm.jsx
@@ -25,16 +25,16 @@ function ProjectForm() {
             <div className="project-form">
                 <Form ref={form} onSubmit={sendEmail}>
                 <Row>
-                    <Col className='project--form-col' md={6}>
+                    <Col className='project--form-col' md={6} xs={6}>
                         <Form.Group className="contact-field" controlId="formBasicEmail" hasValidation>
                             <Form.Label>First name *</Form.Label>
-                            <Form.Control type="text" name='project_first_name' placeholder='e.g. Sophia' required/>
+                            <Form.Control minlength="3" type="text" name='project_first_name' placeholder='e.g. Sophia' required/>
                         </Form.Group>
                     </Col>
-                    <Col className='project--form-col'  md={6}>
+                    <Col className='project--form-col'  md={6} xs={6}>
                         <Form.Group className="contact-field" controlId="formBasicEmail" hasValidation>
                             <Form.Label>Last name *</Form.Label>
-                            <Form.Control type="text" name='project_last_name' placeholder='e.g. Millan' required/>
+                            <Form.Control minlength="3" type="text" name='project_last_name' placeholder='e.g. Millan' required/>
                         </Form.Group>
                     </Col>
                 </Row>
@@ -42,7 +42,7 @@ function ProjectForm() {
                     <Col className='project--form-col' md={12}>
                         <Form.Group className="contact-field" controlId="formBasicEmail" hasValidation>
                             <Form.Label>Your email *</Form.Label>
-                            <Form.Control type="email" name='project_email' placeholder='e.g. sophia@example.com' required/>
+                            <Form.Control minlength="3" type="email" name='project_email' placeholder='e.g. sophia@example.com' required/>
                         </Form.Group>
                     </Col>
                 </Row>
@@ -50,7 +50,7 @@ function ProjectForm() {
                     <Col className='project--form-col' md={12}>
                         <Form.Group className="contact-field" controlId="formBasicEmail" hasValidation>
                             <Form.Label>Company name *</Form.Label>
-                            <Form.Control type="text" name='project_company' placeholder='e.g. Example Company' required/>
+                            <Form.Control minlength="3" type="text" name='project_company' placeholder='e.g. Example Company' required/>
                         </Form.Group>
                     </Col>
                 </Row>
@@ -84,7 +84,7 @@ function ProjectForm() {
                 </Form>
             </div>
             <div className="policy-note">
-                <p>By clicking on “Send Inquiry” button, you agree to our Privacy Policy, 
+                <p>By clicking on “Send Inquiry” button, you agree to our <em className='underlined-text'>Privacy Policy</em>, 
                     and allow Terriy to use this information for marketing purposes.
                 </p>
             </div>
--- src/components/pages/projectstart/Hero.jsx
+++ src/components/pages/projectstart/Hero.jsx
@@ -9,8 +9,20 @@ function Hero() {
     <section className='hero__projectstart top--spacing'>
         <Container>
             <Row>
-                <Col md={6}>1</Col>
-                <Col md={6}>
+                <Col lg={6} className='col-hero__content'>
+                    <div className="txt-container">
+                        <div className="headings-box">
+                            <h2 className='h2-default'>We’re here to make your world better.</h2>
+                            <h3 className='h3-default'>Let’s collaborate.</h3>
+                        </div>
+                        <p className='p-default'>We believe in collaboration and working together to make the world a better place. 
+                        We value your input, feedback, and ideas, and we encourage you to get in touch with us. 
+                        Whether you have a question, a suggestion, or simply want to say hello, we're here to listen.</p>
+                        <p className='p-default p-bolded mobile--hidden'>We understand that communication is key, which is why we've provided this platform for you to connect with us. 
+                        We appreciate your time and look forward to hearing from you.</p>
+                    </div>
+                </Col>
+                <Col lg={6} className='col-hero__form'>
                     <ProjectForm/>
                 </Col>
             </Row>
